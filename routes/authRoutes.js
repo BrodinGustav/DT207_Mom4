31255fcea1bdd8edd6cffa4b512eb20b4c6fd364
Gustav Brodin <brodin.gustav@gmail.com>
2024-05-02 14:27:10 +0200

    Commit 2 - Skapat hantering för registrering med tillhörande kontrollsatser. Skapat hantering för login med tillhörande kontrollsatser. Nästa steg = skapa databas

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,13 +6,44 @@ const router = express.Router();                //Tillgång till routern som ins
 
 //Skapa ny användare
 router.post("/register", async (req, res) => {
-    console.log("Register call...");
+   try {
+        const {username, password } = req.body;   //Läser in vad vi får skickat i requesten till aktuell URL
+
+        //Validera input
+        if(!username || !password) {
+            return res.status(400).json({error: "Invalid input, send username and password"});      //OBS! Skapa error-array som lagrar de olika felmeddelandena!
+        }
+
+        //Correct input - save user
+        //OBS! Här läggs koden för lagring till databas! 
+        res.status(200).json({message: "User created"});
+
+   }catch(error){
+    res.status(500).json({error: "Server error"});
+   }
 });
 
 
 //Logga in användare
 router.post("/login", async (req, res) => {
-    console.log("Login call...")
+        try{
+            const {username, password } = req.body;   
+
+            //Validera input
+        if(!username || !password) {
+            return res.status(400).json({error: "Invalid input, send username and password"});      //OBS! Skapa error-array som lagrar de olika felmeddelandena!
+        }
+
+        //Check credentials (OBS! Korregera så det ej är bestämda värden!)
+        if(username === "Gustav" && password === "1234") {
+            res.status(200).json({message: "Login successful"});
+        }else{
+            res.status(401).json({error: "Invalid username/password"});
+        }
+
+    }catch(error) {
+            res.status(500).json({error: "Server error"});
+        }
 });
 
 module.exports = router;                        //Lägger all kod till app.use i server.js
